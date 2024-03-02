88434ddc901f682f401314f7992122d010a31914
Dima Danilchenko <switch57dima@yandex.ru>
2024-03-02 10:56:42 +0300

    header start

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,22 @@
+import './Header.css';
+import headerImage from '../../assets/logo.svg';
+import headerButtonLeft from '../../assets/header-button-left.svg';
+import headerButtonRight from '../../assets/header-button-right.svg';
 
 const Header = () => {
-  // TODO: сделать заливку общего фона
-  return (
-    <>
-      <p>Header</p>
-    </>
-  );
+    // TODO: сделать заливку общего фона
+    return (
+        <header className="header">
+            <div className="header-logo">
+                <img src={headerImage} className="header-logo__image" alt="Button close" />
+                <p className="header-logo__text">CRM Ambassador</p>
+            </div>
+            <div className="header__buttons">
+                <img src={headerButtonLeft} className="header__button_left" alt="Button close" />
+                <img src={headerButtonRight} className="header__button_right" alt="Button close" />
+            </div>
+        </header>
+    );
 };
 
 export default Header;
