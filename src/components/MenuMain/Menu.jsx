e80d3758f8ee2b0f512f7208611722da2f9ee6eb
levinnsk <stas@uglanov.ru>
2024-03-07 16:36:26 +0700

    Мерж

--- src/components/MenuMain/Menu.jsx
+++ src/components/MenuMain/Menu.jsx
@@ -33,6 +33,9 @@ export default function Menu({ open }) {
             <MenuButton LinkComponent={NavLink} to="/analytics" sx={{ marginBottom: '20px' }} startIcon={<ChartPie />} open={open}>
                 {open ? 'Аналитика' : ''}
             </MenuButton>
+            <MenuButton LinkComponent={NavLink} to="/test" sx={{ marginBottom: '20px' }} startIcon={<ChartPie />} open={open}>
+                {open ? 'Тест' : ''}
+            </MenuButton>
         </Stack>
     );
 }
